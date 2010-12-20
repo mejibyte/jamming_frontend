6a5d0bbe265fcb09542df830a4d7f6c65aa452ac
Andrés Mejía <andmej@gmail.com>
2010-12-20 22:49:20 +0100

    First version of chords

--- jamming.rb
+++ jamming.rb
@@ -4,3 +4,8 @@ require 'sinatra'
 get '/' do
   "Hello from Sinatra on Heroku!"
 end
+
+get '/chords/:chord' do |chord|
+  data = Jamming::Chord.new(chord).to_png(:label => params[:label])
+  send_data data, :type => "image/png", :disposition => "inline"
+end
