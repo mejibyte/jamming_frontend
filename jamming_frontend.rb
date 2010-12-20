6c6773743b283be215a399846dc728fdf09c735a
Andrés Mejía <andmej@gmail.com>
2010-12-20 23:34:21 +0100

    #send_data doesn't exist anymore. Let's try this.

--- jamming_frontend.rb
+++ jamming_frontend.rb
@@ -7,5 +7,6 @@ end
 
 get '/chords/:chord' do |chord|
   data = Jamming::Chord.new(chord).to_png(:label => params[:label])
-  send_data data, :type => "image/png", :disposition => "inline"
+  content_type("image/png")
+  data
 end
